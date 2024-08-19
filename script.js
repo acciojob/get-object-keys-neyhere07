64ed56b7235142a67b94118d9d57850e20dfd387
Neyna Nayak <140084295+neyhere07@users.noreply.github.com>
2024-08-19 20:27:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+
 //your JS code here. If required.
+// Create a student object with a name property
+const student = {
+  name: 'Alice'
+};
+
+// Add getKeys() method to Object prototype
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Using the getKeys() method on the student object
+console.log(student.getKeys()); // Output: ['name']
